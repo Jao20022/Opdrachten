46e6e2723b9662484e0e1bfdbf3a96f945c24b27
Just <just.oudheusden@student.hu.nl>
2022-02-09 11:46:36 +0100

    Create 2 Lijstcheck.py

---
+++ Blok C/Structured Programming/Formatieve Opdrachten/2 Lijstcheck.py
@@ -0,0 +1,33 @@
+from re import X
+from tkinter.messagebox import RETRY
+
+
+def count(Getal, Lijst):
+    Aantal = 0
+    for i in Lijst:
+        if i == Getal:
+            Aantal = Aantal+1
+    return Aantal
+
+
+def GrootsteVerschil(Lijst):
+    Max = 0
+    for i in range(len(Lijst)-1):
+        Verschil = Lijst[i]- Lijst[i+1]
+        if Verschil < 0:
+            Verschil = Verschil * 1
+        if Verschil > Max:
+            Max = Verschil
+    return Max
+
+def Eisen(Lijst):
+    Nullen = count(0, Lijst)
+    Enen = count(1, Lijst)
+    if Enen > Nullen and Nullen <= 12:
+        return True
+    else:
+        return False
+    
+
+
+
