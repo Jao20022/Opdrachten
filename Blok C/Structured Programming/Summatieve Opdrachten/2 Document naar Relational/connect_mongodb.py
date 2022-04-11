d373027f606c75f19479aae7a0a5fd7df00d29a8
Just Oudheusden <just.oudheusden@student.hu.nl>
2022-04-11 10:07:17 +0100

    Revert "Opdracht 2 & 3"
    This reverts commit f321a9a3fcc80e4cae70a37e90fd69f0945265cd.

--- Blok C/Structured Programming/Summatieve Opdrachten/2 Document naar Relational/connect_mongodb.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient()
-db = client.[database]
-mongo_profiles = db.[profile collection]
-mongo_sessions = db.[sessions collection]
-mongo_products = db.[products collection]
--- Blok C/Structured Programming/Summatieve Opdrachten/2 Document naar Relational/connect_postgresql.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-con = psycopg2.connect(
-    host="localhost",
-    database = "huwebshop",
-    user = "postgres",
-    password = "password"
-)
